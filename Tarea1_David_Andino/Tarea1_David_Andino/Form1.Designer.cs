119a6d6f0afbbd948d8d95fec2d2af6cd6f4c070
DavidAndino <123537730+DavidAndino@users.noreply.github.com>
2023-02-03 00:29:24 -0600

    Primer commit del repositorio
    Tarea 1 del  primer parcial

---
+++ Tarea1_David_Andino/Tarea1_David_Andino/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Tarea1_David_Andino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numeroTextBox = new System.Windows.Forms.TextBox();
+            this.verificarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(489, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Verificar número par, impar, positivo o negativo";
+            // 
+            // numeroTextBox
+            // 
+            this.numeroTextBox.Location = new System.Drawing.Point(141, 73);
+            this.numeroTextBox.Name = "numeroTextBox";
+            this.numeroTextBox.Size = new System.Drawing.Size(59, 26);
+            this.numeroTextBox.TabIndex = 3;
+            // 
+            // verificarButton
+            // 
+            this.verificarButton.Location = new System.Drawing.Point(141, 121);
+            this.verificarButton.Name = "verificarButton";
+            this.verificarButton.Size = new System.Drawing.Size(107, 36);
+            this.verificarButton.TabIndex = 4;
+            this.verificarButton.Text = "Verificar";
+            this.verificarButton.UseVisualStyleBackColor = true;
+            this.verificarButton.Click += new System.EventHandler(this.verificarButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 186);
+            this.Controls.Add(this.verificarButton);
+            this.Controls.Add(this.numeroTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("MS Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numeroTextBox;
+        private System.Windows.Forms.Button verificarButton;
+    }
+}
+
---
+++ Tarea1_David_Andino/Tarea1_David_Andino/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea1_David_Andino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void verificarButton_Click(object sender, EventArgs e)
+        {
+            //Imprimiendo mensaje en pantalla para el usuario
+            MessageBox.Show(parOImpar(Convert.ToInt32(numeroTextBox.Text)));//imprimiendo verificacion de numeros pares o impares
+            MessageBox.Show(positivoNegativo(Convert.ToInt32(numeroTextBox.Text)));//imprimiendo verificacion de numeros positivos o negativos
+        }
+        //Funciones creadas 
+        private string parOImpar(int numero)
+        {
+            //Declarando e inicializando variable en vacío
+            string respuesta = "";
+
+            //Iniciando decision doble
+            if (numero % 2 == 0)
+            {
+                respuesta = "El número ingresado es par";
+            }
+            else
+            {
+                respuesta = "El numero ingresado es impar";
+            }
+            return respuesta;//retornando respuesta luego de evaluar la condicion o desicion doble
+        }
+        private string positivoNegativo(int numero)
+        {
+            //Declarando e inicializando variable en vacío
+            string tipoNumero = "";
+
+            //Iniciando condicion anidada
+            if (numero == 0)
+            {
+                tipoNumero = "El cero es un número neutro";
+            }
+            else if (numero > 0)
+            {
+                tipoNumero = "y es positivo";
+            }
+            else
+            {
+                tipoNumero = "y es negativo";
+            }
+            return tipoNumero;
+        }
+    }
+}
